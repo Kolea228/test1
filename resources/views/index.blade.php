5666214a6114a6aa2d448ce4ae9e2dd3ed65cd20
Moraru Marcel <morarumarcel750@gmail.com>
2020-02-14 11:00:34 +0200

    articless

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,7 +32,7 @@
   <!-- Main jumbotron for a primary marketing message or call to action -->
   <div class="jumbotron">
     <div class="container">
-      <h1 class="display-3">{{$HW}}</h1>
+      <h1 class="display-3">{{$hw}}</h1>
       <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
       <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
     </div>
@@ -45,7 +45,7 @@
 
       <div class="col-md-4">
         <h2>{{$article->title}}</h2>
-        <p><img src='{{$article->img}}'></p>
+        <p><img src='{{$article->img}}' width="150px" height="150px"></p>
         <p>{{$article->description}}</p>
         <p>{{$article->text}}</p>
         <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
